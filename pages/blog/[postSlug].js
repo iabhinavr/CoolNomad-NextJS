a2abdc9e511efa0cce25f506eae0c641fecc5b61
Abhinav R <abhinav7r@gmail.com>
2023-01-13 01:56:05 +0530

    css added, dated added to single post page

--- pages/blog/[postSlug].js
+++ pages/blog/[postSlug].js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import SiteHeader from "../../components/SiteHeader";
 import SiteFooter from "../../components/SiteFooter";
 import { getPostSlugs, getSinglePost } from "../../lib/posts";
+import Date from "../../components/Date";
 
 export async function getStaticProps({ params }) {
     const postData = await getSinglePost(params.postSlug);
@@ -51,6 +52,10 @@ export default function Post({ postData, featuredImageUrl }) {
 
                 <div className="container mx-auto h-full flex flex-col justify-center lg:max-w-4xl">
                     <h1 className="text-6xl text-center text-slate-100 relative z-10 py-8 mt-12">{postData.title}</h1>
+
+                    <div className="pb-4 text-slate-100 z-10">
+                        Posted by Abhinav, last updated on <Date dateString={postData.modified} />
+                    </div>
 
                     <div dangerouslySetInnerHTML={{ __html: postData.excerpt }} className="relative z-10 text-left text-slate-200 text-2xl pl-4 border-l-4 border-lime-200"/>
                 </div>
