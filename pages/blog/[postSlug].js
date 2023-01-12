638cf89f87b05715811749d4c3f645ea3ec2b96f
Abhinav R <abhinav7r@gmail.com>
2023-01-13 00:50:13 +0530

    single post page added

--- pages/blog/[postSlug].js
+++ pages/blog/[postSlug].js
@@ -1,5 +1,65 @@
-export default function Post() {
+import Head from "next/head";
+import SiteHeader from "../../components/SiteHeader";
+import SiteFooter from "../../components/SiteFooter";
+import { getPostSlugs, getSinglePost } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+    const postData = await getSinglePost(params.postSlug);
+
+    let featuredImageUrl = "https://wp.abhinavr.com/wp-content/uploads/2022/12/travel_icy-polar_022K.jpg";
+
+    if(postData.featuredImage.node.mediaDetails.sizes[0].sourceUrl) {
+        featuredImageUrl = postData.featuredImage.node.mediaDetails.sizes[0].sourceUrl;
+    }
+
+    return {
+        props: {
+            postData,
+            featuredImageUrl: "url(" + featuredImageUrl + ")",
+        }
+    };
+}
+
+export async function getStaticPaths() {
+    const postSlugs = await getPostSlugs();
+
+    return {
+        paths: postSlugs.map((s) => (
+            {
+                params: {
+                    postSlug: s.slug
+                }
+            }
+        )),
+        fallback: false 
+    }
+}
+
+export default function Post({ postData, featuredImageUrl }) {
     return (
-        <h1>This is a sample post</h1>
+        <>
+        <Head>
+            <title key={postData.slug}>{postData.title}</title>
+            <meta name="description" content={postData.excerpt} key="metadescription" />
+        </Head>
+        <section className="bg-slate-700 bg-opacity-70 absolute w-full z-20">
+            <SiteHeader className="header-single-post z-10 relative" />
+        </section>
+        <article>
+            <section className="hero-area h-[60vh] min-h-[30rem] bg-no-repeat bg-cover bg-center relative" style={{backgroundImage: featuredImageUrl}}>
+                <div className="absolute inset-0 bg-slate-900 opacity-40"></div>
+
+                <div className="container mx-auto h-full flex flex-col justify-center lg:max-w-4xl">
+                    <h1 className="text-6xl text-center text-slate-100 relative z-10 py-8 mt-12">{postData.title}</h1>
+
+                    <div dangerouslySetInnerHTML={{ __html: postData.excerpt }} className="relative z-10 text-left text-slate-200 text-2xl pl-4 border-l-4 border-lime-200"/>
+                </div>
+            </section>
+            <section className="content-area py-8">
+                <div dangerouslySetInnerHTML={{ __html: postData.content }} className="post-content container lg:max-w-4xl mx-auto"/>
+            </section>
+        </article>
+        <SiteFooter />
+        </>
     );
 }
