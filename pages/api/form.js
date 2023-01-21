65e0c561ffd7d34e7bdc70b8cab98f6be7fae7f4
Abhinav R <abhinav7r@gmail.com>
2023-01-21 14:48:35 +0530

    contact page added

---
+++ pages/api/form.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+    const body = req.body;
+
+    if(!body.firstName || !body.email || !body.message) {
+        return res.status(400).json({data: "first name, email, and message fields are required!"});
+    }
+
+    return res.status(200).json({data: "form submitted successfully"});
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,85 @@
+import Head from "next/head";
+import SiteHeader from "../components/SiteHeader";
+import SiteFooter from "../components/SiteFooter";
+import { useState } from "react";
+
 export default function Contact() {
+
+    const [submitStatus, setSumbitStatus] = useState(false);
+    const [responseMessage, setResponseMessage] = useState('');
+    const [alertColor, setAlertColor] = useState('bg-green-500');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        const data = {
+            firstName: event.target.firstName.value,
+            email: event.target.email.value,
+            message: event.target.message.value,
+        }
+    
+        const jsonData = JSON.stringify(data);
+    
+        const response = await fetch('/api/form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: jsonData,
+        });
+    
+        const result = await response.json();
+        console.log(result.data);
+    
+
+        setSumbitStatus(true);
+        setResponseMessage(result.data);
+
+        if(!response.ok) {
+            setAlertColor('bg-red-500');
+        }
+        else {
+            setAlertColor('bg-green-500');
+        }
+    
+    
+    }
+
     return (
-        <h1>Contact Us</h1>
+        <>
+        <Head>
+            <title>Contact Us</title>
+        </Head>
+        <section className="bg-slate-700">
+            <SiteHeader className="header-contact" />
+        </section>
+        <section>
+            <div className="container mx-auto lg:max-w-4xl">
+                <h1 className="text-4xl text-center text-slate-700 py-8">Contact Us</h1>
+                <form className="contact-form" onSubmit={handleSubmit}>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input type="text" id="firstName" name="firstName" />
+
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" />
+
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" id="message"></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
+                {submitStatus ? <SubmissionAlert message={responseMessage} alertColor={alertColor} /> : null}
+            </div>
+            
+        </section>
+        </>
     );
+}
+
+const SubmissionAlert = ({message, alertColor}) => {
+    return (
+        <div className={`${alertColor} py-2 px-4 mt-4 text-slate-100 rounded-md`}>
+            {message}
+        </div>
+    )
 }
